af779773c381537a416d3b16b3f90d186e018ba3
Brendan K <brendanmkelly94@gmail.com>
2019-02-09 10:08:11 -0600

    class work addded up to 2/5

---
+++ 2019/02/07/activities/bestThingsEver/bestThings.js
@@ -0,0 +1,15 @@
+var fs = require("fs");
+
+fs.readFile("best_things_ever.txt", "utf8", function(error, data){
+
+    if(error){
+        console.log(error);
+    }
+
+    var dataArr = data.split(",");
+
+    for (var i = 0; i < dataArr.length; i++){
+        console.log(dataArr[i]);
+    }
+})
+
